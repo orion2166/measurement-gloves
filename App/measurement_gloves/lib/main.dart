d74e336b244e335aa147a91008b9de73d314df7c
Kornkarnjana Sapianchai <golfsapianchai@Kornkarnjanas-MacBook-Pro.local>
2020-10-20 11:25:49 +0700

    added monitoring mode resizable circles test

--- App/measurement_gloves/lib/main.dart
+++ App/measurement_gloves/lib/main.dart
@@ -1,62 +1,76 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import './GloveApp.dart';
-import './heartbeat.dart';
+import 'circle_painter.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/': (context) => FirstScreen(),
-        //'/GloveApp': (context) => GloveApp(),
-        '/heartbeat': (context) => Heartbeat(),
-      },
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class FirstScreen extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+// state class
+class _MyHomePageState extends State<MyHomePage> {
+  // state variable
+  double forceVal = 0;
+
+  // The State class must include this method, which builds the widget
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Gloves Nearby'),
+        title: Text('Monitor mode - Test'),
+        backgroundColor: Color(0xFF444444),
       ),
-      body: ListView(
-        children: ListTile.divideTiles(
-          context: context,
-          tiles: [
-            ListTile(
-              title: Text('Gloves 1'),
-              trailing: Icon(Icons.keyboard_arrow_right),
-              onTap: () {
-                //Navigator.of(context).pushNamed('/GloveApp');
-                Navigator.of(context).pushNamed('/heartbeat');
-              },
-            ),
-            ListTile(
-              title: Text('Gloves 2'),
-              trailing: Icon(Icons.keyboard_arrow_right),
-              onTap: () {
-                //Navigator.of(context).pushNamed('/GloveApp');
-                Navigator.of(context).pushNamed('/heartbeat');
-              },
-            ),
-            ListTile(
-              title: Text('Gloves 3'),
-              trailing: Icon(Icons.keyboard_arrow_right),
-              onTap: () {
-                //Navigator.of(context).pushNamed('/GloveApp');
-                Navigator.of(context).pushNamed('/heartbeat');
-              },
-            ),
-          ],
-        ).toList(),
-      ),
+      body: ListView(children: <Widget>[
+        Text(
+          'Canvas',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20, height: 2),
+        ),
+        Container(
+            width: 400,
+            height: 400,
+            child: Circle(
+              center: {"x": 100, "y": 100},
+              radius: forceVal,
+            )),
+        Container(
+          child: RaisedButton(
+            child: Text('Test'),
+            color: Colors.blueAccent,
+            onPressed: _changeSize,
+          ),
+        )
+      ]),
     );
+  }
+
+  // change circle size when button pressed
+  _changeSize() {
+    setState(() {
+      if (forceVal == 0) {
+        forceVal = 10;
+      } else if (forceVal == 10) {
+        forceVal = 20;
+      } else if (forceVal == 20) {
+        forceVal = 30;
+      } else {
+        forceVal = 0;
+      }
+    });
   }
 }

aad86f772c9b6f2747a68665adac319857938b55
Anuj Parakh <anuj@anujinfotech.com>
2020-10-22 10:45:59 -0500

    added ble dependencies and changed opening screen logic

--- App/measurement_gloves/lib/main.dart
+++ App/measurement_gloves/lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
 
 import './GloveApp.dart';
 import './heartbeat.dart';
@@ -9,12 +13,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/': (context) => FirstScreen(),
-        //'/GloveApp': (context) => GloveApp(),
-        '/heartbeat': (context) => Heartbeat(),
-      },
+      color: Colors.lightBlue,
+      home: StreamBuilder<BluetoothState>(
+          stream: FlutterBlue.instance.state,
+          initialData: BluetoothState.unknown,
+          builder: (c, snapshot) {
+            final state = snapshot.data;
+            if (state == BluetoothState.on) {
+              return FirstScreen();
+            }
+            return BluetoothOffScreen(state: state);
+          }),
     );
   }
 }
@@ -56,6 +65,39 @@ class FirstScreen extends StatelessWidget {
             ),
           ],
         ).toList(),
+      ),
+    );
+  }
+}
+
+// Show this when Bluetooth cannot be used
+class BluetoothOffScreen extends StatelessWidget {
+  const BluetoothOffScreen({Key key, this.state}) : super(key: key);
+
+  final BluetoothState state;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Icon(
+              Icons.bluetooth_disabled,
+              size: 200.0,
+              color: Colors.white54,
+            ),
+            Text(
+              'Bluetooth Adapter is ${state != null ? state.toString().substring(15) : 'not available'}.',
+              style: Theme.of(context)
+                  .primaryTextTheme
+                  .subhead
+                  .copyWith(color: Colors.white),
+            ),
+          ],
+        ),
       ),
     );
   }
